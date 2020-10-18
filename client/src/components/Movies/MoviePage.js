bd442e0a716240c8c7b602bdee57578c61d7ea51
e-a-w- <eawatkins.0@gmail.com>
2020-10-18 14:12:43 -0400

    movie search and result pages

--- client/src/components/Movies/MoviePage.js
+++ client/src/components/Movies/MoviePage.js
@@ -1,10 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./movie.css";
+import { Card, Table } from "react-bootstrap";
+import axios from "axios";
 
-const MoviePage = () => {
+const MoviePage = ({ match }) => {
+  const { id } = match.params;
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios(`http://www.omdbapi.com/?i=${id}&apikey=OMDB_KEY`)
+      .then(({ data }) => setMovie(data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
   return (
-    <div>
-      <h1>This is the Movie Results Page</h1>
-    </div>
+    <>
+      <h1>{movie?.Title}</h1>
+      <Card key={movie?.imdbID} style={{ margin: "20px" }}>
+        <Card.Body id="movie-card">
+          <Card.Img
+            id="movie-img"
+            className="mr-3 mb-3"
+            src={movie?.Poster}
+            alt="movie"
+          />
+          <Table id="movie-table" striped bordered hover>
+            <thead>
+              <tr>
+                <th>&nbsp;</th>
+                <th>Info</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Released</td>
+                <td>
+                  {movie?.Released}, {movie?.Language}, {movie?.Country}
+                </td>
+              </tr>
+              <tr>
+                <td>Rated</td>
+                <td>{movie?.Rated}</td>
+              </tr>
+              <tr>
+                <td>Runtime</td>
+                <td>{movie?.Runtime}</td>
+              </tr>
+              <tr>
+                <td>Genre</td>
+                <td>{movie?.Genre}</td>
+              </tr>
+              <tr>
+                <td>Team</td>
+                <td>
+                  <h6>Writers: </h6>
+                  <p>{movie?.Writer}</p>
+                  <hr />
+                  <h6>Director: </h6>
+                  <p>{movie?.Director}</p>
+                  <hr />
+                  <h6>Actors:</h6>
+                  <p>{movie?.Actors}</p>
+                </td>
+              </tr>
+              <tr>
+                <td>Plot</td>
+                <td>{movie?.Plot}</td>
+              </tr>
+              <tr>
+                <td>Awards</td>
+                <td>{movie?.Awards}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
--- client/src/components/Search/SearchResultsPage.js
+++ client/src/components/Search/SearchResultsPage.js
@@ -1,29 +1,27 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import { Card, Button } from "react-bootstrap";
-import movieImg from "../../assets/movieImg.jpg";
 
-const SearchResultsPage = () => {
+const SearchResultsPage = ({ history }) => {
   const { searchResults } = useContext(AppContext);
-  const map = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  console.log(searchResults);
 
   return (
     <>
       <h1 className="mb-5">Search Results:</h1>
       <div className="profile-flexbox">
-        {map?.map((movie, i) => {
+        {searchResults?.map((movie, i) => {
           return (
-            <Card key={i} style={{ maxWidth: "15rem", margin: "20px" }}>
-              <Card.Img variant="top" src={movieImg} alt="movie" />
-              <Card.Body>
-                <Card.Title className="text-center">Movie Title:</Card.Title>
-                <Card.Text>Movie Description goes here</Card.Text>
+            <Card
+              key={movie.imdbID}
+              style={{ maxWidth: "15rem", margin: "20px" }}
+            >
+              <Card.Img variant="top" src={movie.Poster} alt="movie" />
+              <Card.Body className="text-center">
+                <Card.Title>{movie.Title}</Card.Title>
+                <Button onClick={() => history.push(`/movie/${movie.imdbID}`)}>
+                  Link to Movie
+                </Button>
               </Card.Body>
-              <Card.Footer className="text-center">
-                <Button>Link to Movie</Button>
-              </Card.Footer>
             </Card>
           );
         })}
